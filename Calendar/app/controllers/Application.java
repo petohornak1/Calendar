2243ae577852f63533c9476bde11151124bf6888
petohornak1 <petohornak1@gmail.com>
2012-11-19 00:55:35 +0100

    Nedela vecar

--- Calendar/app/controllers/Application.java
+++ Calendar/app/controllers/Application.java
@@ -1,14 +1,39 @@
 package controllers;
 
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
+	
+	static Form<User> signInForm = form(User.class);
+	static Form<User> registrationForm = form(User.class);
   
   public static Result index() {
-    return ok(index.render("Hello world!"));
+    return ok(index.render(signInForm, registrationForm));
   }
+  
+  public static Result signIn() {
+		Form<User> filledForm = signInForm.bindFromRequest();
+		User signingInUser = filledForm.get();						
+		if (User.signIn(signingInUser.email, signingInUser.password)) {
+			return ok(calendar.render("Vitajte"));
+		} else {			
+			return ok(index.render(signInForm, registrationForm));
+		}
+	}
+  
+  public static Result signUp() {
+		Form<User> filledForm = registrationForm.bindFromRequest();
+		User newUser = filledForm.get();		
+		if (!User.isEmailUnique(newUser.email)) {
+			return ok(index.render(signInForm, registrationForm));
+		}		
+		User.save(newUser);
+		return index();
+	}
   
 }
---
+++ Calendar/app/models/User.java
@@ -0,0 +1,53 @@
+package models;
+
+import play.db.ebean.*;
+import javax.persistence.*;
+
+@Entity
+public class User extends Model {
+
+	public static Finder<Long, User> find = new Finder(Long.class,User.class);
+
+	@Id
+	public Long id;
+
+	@Column(nullable = false, unique = true)
+	public String email;
+
+	@Column(nullable = false)
+	public String password;
+
+	@Transient
+	public String rePassword;
+
+	@Column(nullable = false)
+	public String firstName;
+
+	@Column(nullable = false)
+	public String lastName;
+
+	@Column(nullable = false)
+	public String birthday;
+
+	public static boolean isEmailUnique(String email) {
+		if (find.where().like("email", email).findUnique() == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void save(User user) {
+		user.save();
+	}
+
+	public static boolean signIn(String email, String password) {
+		User user = find.where().like("email", email)
+				.like("password", password).findUnique();
+		if (user == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
